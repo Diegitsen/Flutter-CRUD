024e3cc00e42617b2aa47e3e638c1ebc8a00e2ae
diegitsen <diegitsen@gmail.com>
2019-10-10 22:08:09 +0200

    first comm

---
+++ lib/model/product.dart
@@ -0,0 +1,12 @@
+class Product
+{
+  String _id;
+  String _name;
+  String code;
+  String description;
+  String Price;
+  String stock;
+
+  Product(this._id, this._name, this.code, this.description, this.Price, this.stock);
+
+}
